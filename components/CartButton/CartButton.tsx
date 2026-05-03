f3f3d2b72f1d5fb0b160da9c7acecc45646c8d22
Damian Junkiewicz <junkiewicz.damian@gmail.com>
2026-05-03 22:18:57 +0200

    Update: Cart component and refactor

--- components/CartButton/CartButton.tsx
+++ components/CartButton/CartButton.tsx
@@ -10,8 +10,9 @@ type Props = {
 
 export default function CartButton({cartCounter, clearCartFn}: Props) {
   return (
-    <div className={styles.mainContainer}>
-        <button className={styles.cartButton}>{cartCounter}<img src="shopping-cart.svg" onClick={clearCartFn}/></button>
+    <div className={styles.mainContainer} onClick={clearCartFn}>
+        <div className={styles.cartButton}><img src="shopping-cart.svg"/></div>
+        <div className={styles.counter}>{cartCounter}</div>
     </div>
   );
 }
